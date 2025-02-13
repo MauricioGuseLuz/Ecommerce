ad863a4172896255c635a2367df4e1055e69d0fd
Mauricio Guse <soldadorasta23@gmail.com>
2025-02-13 18:47:11 -0300

    add coment

--- src/main/java/com/ecommerce/api/entities/Category.java
+++ src/main/java/com/ecommerce/api/entities/Category.java
@@ -19,7 +19,7 @@ public class Category {
     private String name;
 
     @ManyToMany(mappedBy = "categories")
-    private Set<Product> products = new HashSet<>();//oila
+    private Set<Product> products = new HashSet<>();//oilad
 
 
 }
